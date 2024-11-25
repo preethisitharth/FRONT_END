29150a0e0a64fce260f1a1f72f3d2778f8bc9c97
preethisitharth <preethisitharth@gmail.com>
2024-11-25 10:35:42 +0530

    JavaScript

---
+++ JavaScript/Function.js
@@ -0,0 +1,14 @@
+function preethi()
+{
+    alert("welcome function calling")
+}
+function sum(a,b)
+{
+    let result=a+b;
+    alert("A values is:"+a+"B Is values is:"+b+"a+b "+result)
+}
+
+let print=(name)=> 
+{
+    document.writeln("My name is :"+name)
+}
